df6cf49d0186e71f37892c15811322e36069b04e
Mehmet Can Girgin <119158579+Casm9@users.noreply.github.com>
2025-03-08 12:36:28 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -14,25 +14,25 @@ export default function MainScreen() {
   const router = useRouter();
   const { videos } = useVideoStore();
 
-  const renderVideoItem = ({ item }: { item: VideoItem }) => (
-    <View style={{ height:250 }}>
-      <TouchableOpacity
-        style={{ padding: 10, margin: 10, backgroundColor: 'lightgray' }}
-        onPress={() => router.push({
-          pathname: '/details',
-          params: { videoId: item.id }
-        })}
-      >
-        <Text style={{ textAlign: 'center' }}>
-          {item.name}
-        </Text>
-        <VideoPlayer
-          source={{ uri: item.uri }}
-          style={{ width: '100%', height: 200 }}
-        />
-      </TouchableOpacity>
-
+  const renderVideoItem = ({ item }: {item: VideoItem}) => (
+    <View style={{ height: 250 }}>
+    <TouchableOpacity 
+      style={{ padding: 10, margin: 10, backgroundColor: 'lightgray' }}
+      onPress={() => router.push({
+        pathname: '/details',
+        params: { videoId: item.id }
+      })}
+    >
+      <Text style={{ textAlign: 'center' }}>
+        {item.name}
+      </Text>
+      <VideoPlayer
+        source={{ uri: item.uri }}
+        style={{ width: '100%', height: 200 }}
+      />
+    </TouchableOpacity>
     </View>
+
   );
 
   return (
@@ -51,11 +51,11 @@ export default function MainScreen() {
         />
       )}
 
-      <TouchableOpacity
+      <TouchableOpacity 
         style={{ padding: 10, backgroundColor: 'lightblue', margin: 10 }}
         onPress={() => router.push('/crop')}
       >
-        <Text style={{ textAlign: 'center' }}>Crop New Video</Text>
+        <Text style={{textAlign: 'center'}}>Crop New Video</Text>
       </TouchableOpacity>
     </View>
   );
